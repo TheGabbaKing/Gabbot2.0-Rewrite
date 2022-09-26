a837b3dc6926f6dee97a5fc12817cd93bdc79c53
Gabba <thegabbaking@gmail.com>
2022-09-26 23:52:36 +1000

    Updated WEBM converter
    Added errors and catch cases to WEBM converter

--- cogs/commands.py
+++ cogs/commands.py
@@ -8,20 +8,16 @@ class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.Cog.listener()
+    @commands.Cog.listener() #INITIALISER
     async def on_ready(self):
         print(f'{self.__class__.__name__} Cog is ready')
 
-    @commands.command()
+    @commands.command() #SYNC SLASH COMMANDS
     async def sync(self, ctx) -> None:
         fmt = await ctx.bot.tree.sync(guild=ctx.guild)
         await ctx.send(f"Synced {len(fmt)} commands to {ctx.guild.name}.")
 
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send("This command is a test")
-    
-    @commands.Cog.listener()
+    @commands.Cog.listener() #INSTAGRAM & TWITTER URL PARSER
     async def on_message(self, ctx):
         if ctx.content.startswith("https://www.instagram.com") or ctx.content.startswith("https://twitter.com"):
             url = ctx.content.split("?") #splits URL into parts
@@ -39,26 +35,23 @@ class Commands(commands.Cog):
                 await ctx.channel.send(f"{url[0]}")
             await ctx.delete()
 
-
-    @commands.command()
+    @commands.command() #WEBM CONVERTER
     async def save(self, ctx, arg1:Optional[str] = None):
-        if arg1 == None and ctx.message.attachments:
+        if arg1 == None and ctx.message.attachments and ctx.message.attachments[0].url.endswith("webm"):
             print("Attachment found")
             url = ctx.message.attachments[0].url
-        elif arg1.startswith("https://cdn.discordapp.com"):
+        elif arg1.startswith("https://cdn.discordapp.com") and arg1.endswith(".webm"):
             print("URL found")
             url = arg1
         else:
-            print("sorry mate idk")
-            return
-        print("starting request")
+            await ctx.send("Sorry your file isn't supported yet. Only webm is supported right now.")
         r = requests.get(url, stream=True)
         fileName = f'files/{str(uuid.uuid4())}' + '.mp4'
         with open(fileName, 'wb') as outFile:
-            print("Saving file: " + fileName)
-            shutil.copyfileobj(r.raw, outFile)
-            await ctx.send(file=discord.File(f'{fileName}'))
-        
-
+            async with ctx.typing():
+                print("Saving file: " + fileName)
+                shutil.copyfileobj(r.raw, outFile)
+                await ctx.send(file=discord.File(f'{fileName}'))
+            
 async def setup(bot):
     await bot.add_cog(Commands(bot), guilds=[discord.Object(id=524776650945200138)])
