9df27ee7e42f333c59443083bce90ebbb31246d1
Gabba <thegabbaking@gmail.com>
2022-09-26 17:22:26 +1000

    Instagram/Twitter link editor
    Updates instagram and twitter links to remove sharing URL and converts twitter vids to fxtwitter

--- cogs/commands.py
+++ cogs/commands.py
@@ -21,7 +21,24 @@ class Commands(commands.Cog):
     @commands.command()
     async def test(self, ctx):
         await ctx.send("This command is a test")
-
+    
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.content.startswith("https://www.instagram.com") or ctx.content.startswith("https://twitter.com"):
+            url = ctx.content.split("?") #splits URL into parts
+            if ctx.author.id == self.bot.user.id or ctx.author.id != 487110546223661076: #checks if the user who sent it was the bot or NOT me
+                return
+            elif ctx.content.startswith("https://twitter.com"):
+                cutURL = url[0]
+                if ctx.content.endswith("no"):
+                    noURL = cutURL[:-2]
+                    await ctx.channel.send(f"{noURL}")
+                else:
+                    newURL = cutURL[:8] + "fx" + cutURL[8:]
+                    await ctx.channel.send(f"{newURL}")
+            else:
+                await ctx.channel.send(f"{url[0]}")
+            await ctx.delete()
 
 async def setup(bot):
     await bot.add_cog(Commands(bot), guilds=[discord.Object(id=524776650945200138)])
