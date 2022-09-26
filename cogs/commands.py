64f4104351b635712005df545f5c63075bb42c30
Gabba <thegabbaking@gmail.com>
2022-09-26 17:21:52 +1000

    Reload cogs
    Allows me to reload cogs with a command

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,4 +1,7 @@
 import discord
+import os
+import asyncio
+from discord import app_commands
 from discord.ext import commands
 
 class Commands(commands.Cog):
@@ -10,5 +13,15 @@ class Commands(commands.Cog):
     async def on_ready(self):
         print(f'Commands Cog is ready')
 
+    @commands.command()
+    async def sync(self, ctx) -> None:
+        fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+        await ctx.send(f"Synced {len(fmt)} commands to {ctx.guild.name}.")
+
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("This command is a test")
+
+
 async def setup(bot):
-    await bot.add_cog(Commands(bot))
+    await bot.add_cog(Commands(bot), guilds=[discord.Object(id=524776650945200138)])
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="g.", intents=intents)
+bot = commands.Bot(command_prefix="g.", intents=intents, application_id="1020217647973027870")
 
 
 @bot.event
@@ -22,6 +22,22 @@ async def on_message(ctx):
     if ctx.author.id == bot.user.id:
         return
     await ctx.channel.send("What up bitch")
+
+@bot.command()
+@commands.is_owner()
+async def reload(ctx):
+    async with ctx.typing():
+        print("trying to reload")
+        for ext in os.listdir("./cogs/"):
+            if ext.endswith(".py") and not ext.startswith("_"):
+                try:
+                    print("found extensions")
+                    await bot.reload_extension(f"cogs.{ext[:-3]}")
+                    await ctx.send(f"{ext[:-3]} Cog Reloaded")
+                except Exception as e:
+                    print("didn't find anything")
+                    await ctx.send(f"No cogs were reloaded")
+                await asyncio.sleep(0.5)
 
 async def load():
     for filename in os.listdir('./cogs'):
